5534540fbc566782a2dfa2b9cee3d349a3790e85
smo-key <pachachura.arthur@gmail.com>
2014-03-07 10:22:41 -0600

    Header Updated - Click Events Needed

--- js/index.js
+++ js/index.js
@@ -11,13 +11,15 @@ function updateContainer() {
     if ($containerWidth <= 680) {
         $("title-div").addClass("title-div-comp");
         $("title-items-div").addClass("title-items-div-comp");
-        $("title-name").addClass("title-name-comp");
         var $aWidth = ($containerWidth - $("title-items-div").width())/2;
         $("title-items-div").css("right", $aWidth);
+        $("title-item").addClass("title-item-comp");
+        $("title-name").addClass("title-name-comp");
     }
     if ($containerWidth > 681) {
         $("title-div").removeClass("title-div-comp");
         $('title-items-div').removeClass("title-items-div-comp");
+        $("title-item").removeClass("title-item-comp");
         $("title-name").removeClass("title-name-comp");
         $("title-items-div").css("right","36px");
     }
