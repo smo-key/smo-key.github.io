88ce9ccd7a2d04c4f753eb6812dba33c5155c04d
smo-key <smo-key@github.com>
2014-03-18 21:30:16 -0500

    Much Fixed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
-
+  updateContainer();
+  $(window).resize(function() {
+    updateContainer();
+  });
 });
+
+function updateContainer() {
+  //var $containerWidth = $(window).width();
+  var $containerHeight = $(window).height();
+  var $aloc = ($containerHeight - $("slide1-text").width())/2;
+  $("slide1-text").css("top",$aloc);
+  //$(document).add(alert("Hello World 2!"));
+}
 
 window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       ||
