b01ee1fae5320df477da454d46cabf1db0d8da61
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-06-17 12:12:29 -0500

    Update Hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,38 @@
 $(document).ready(function () {
   firstLoad();
   updateContainer();
+  updateScroll();
+  
   $(window).resize(function() {
     updateContainer();
   });
+  
+  $(window).scroll(function() {
+     updateScroll();
+  });
+  
 });
 
 function firstLoad()
 {
-  $("#copyright").text('Copyright © ' + new Date().getFullYear().toString() + ' Arthur Pachachura');  
+  $("#copyright").text('Copyright © ' + new Date().getFullYear().toString() + ' Arthur Pachachura');
+}
+
+function updateScroll() {
+  var s = $(document).scrollTop();
+  $(".header").css("background-color","rgba(242, 242, 242, " + (coerce(s*2,0,70)/100).toString() + ")");
+  $("#heroslide").css("-webkit-filter","blur(" + (coerce((s - 25)*2.5,0,500)/100).toString() + "px)");
 }
 
 function updateContainer() {
-  var $cW = $(window).width();
-  var $cH = $(window).height();
-  if ($cH < 400) { $cH = 400; }
-  var $stH = $cH;
-  if (($cW < 700) && ($cH > 400)) { $stH = 400; }
-  $(".slidetext-center").css('height',$stH);
-  $(".slidetext-center").css('width',$cW);
-  $(".slidetext-static").css('width',$cW);
-  $(".slidetext-top").css('height',$stH);
-  $(".slidetext-top").css('width',$cW);
-  var $s4H = $cW / 4 - 20;
-  $(".slide4-item").css('height',$s4H);
-  $(".slide4-item").css('padding-top',"0");
+//  var $cW = $(window).width();
+//  var $cH = $(window).height();
+}
+
+function coerce(int, min, max) {
+  if (int > max) { return max; }
+  if (int < min) { return min; }
+  return int;
 }
 
 window.requestAnimFrame = (function(){
