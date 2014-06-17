5ff377e76d34e6609354a393266c79b8aba3a7cb
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-06-17 16:40:18 -0500

    Smooth Scrolling Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
   firstLoad();
+  updateResize();
   updateScroll();
+  smoothscroll();
+
+  $(window).resize(function() {
+    updateResize();
+  });
   
   $(window).scroll(function() {
-     updateScroll();
+    updateScroll();
   });
   
 });
@@ -13,11 +19,32 @@ function firstLoad()
   $("#copyright").text('Copyright © ' + new Date().getFullYear().toString() + ' Arthur Pachachura');
 }
 
+function updateResize() {
+  
+}
+
+function smoothscroll() {
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 500);
+       return false;
+      }
+    }
+  });
+}
+
 function updateScroll() {
   var s = $(document).scrollTop();
   $(".header").css("background-color","rgba(242, 242, 242, " + (coerce(s - 50,0,70)/100).toString() + ")");
-  $("#heroslide").css("-webkit-filter","blur(" + (coerce((s - 40)*2.5,0,750)/100).toString() + "px)");
-  if (s > $("#heroslide").innerHeight() + 16) {$("#headercontainer").removeClass("headerlesser").addClass("headergreater"); $("#headeravatar").addClass("headeravataractive"); $("#heropic").addClass("herozero"); $("#headeravatartext").addClass("headeravataractive"); }
+  $("#profile").css("-webkit-filter","blur(" + (coerce((s - 40)*2.5,0,750)/100).toString() + "px)");
+  if (s > $("#profile").innerHeight() + 16) {$("#headercontainer").removeClass("headerlesser").addClass("headergreater"); $("#headeravatar").addClass("headeravataractive"); $("#heropic").addClass("herozero"); $("#headeravatartext").addClass("headeravataractive"); }
   else {$("#headercontainer").removeClass("headergreater").addClass("headerlesser"); $("#headeravatar").removeClass("headeravataractive"); $("#heropic").removeClass("herozero"); $("#headeravatartext").removeClass("headeravataractive");}
 }
 
@@ -35,3 +62,4 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
           };
 })();
+
